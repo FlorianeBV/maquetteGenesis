22111c37a8b4d9f3a6ec80bc330632a8ed0fa36a
Fetta <fetta.rebah@worldline.com>
2021-05-12 12:03:39 +0200

    Ajout des components dans le fichier routing

--- ng2ecom/src/app/app.routing.ts
+++ ng2ecom/src/app/app.routing.ts
@@ -2,10 +2,16 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+import { HeaderComponent } from './components/header/header.component';
+import { DossierComponent } from './components/dossier/dossier.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 
 const appRoutes: Routes = [
-
+	{ path: 'header', component: HeaderComponent },
+  {path:'footer', component:FooterComponent},
+  {path:'dossier',component:DossierComponent}
 ];
 
 
